441d681da6466efa4aca33c24c8f7f464937e71c
gary21978 <gary21978@126.com>
2020-09-25 17:20:41 +0800

    data type test

--- demo.m
+++ demo.m
@@ -1,5 +1,9 @@
 clear;close all;
-img = imread('stripes.png');
+img = imread('stripes.png');             % uint8
+%img = im2uint16(imread('stripes.png')); % uint16
+%img = im2double(imread('stripes.png')); % double
+%img = im2single(imread('stripes.png')); % single
+
 filtered = FastGlobalSmoothing(img, 0.03, 900);
 subplot(121);imshow(img);title('Original');
 subplot(122);imshow(filtered);title('Filtered');
