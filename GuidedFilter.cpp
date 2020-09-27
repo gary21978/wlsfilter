405e4ac7503ab1d1d11da3193a1bb428d91c71fb
gary21978 <gary21978@126.com>
2020-09-27 15:17:12 +0800

    Update MEX function of guided filter

--- GuidedFilter.cpp
+++ GuidedFilter.cpp
@@ -1,15 +1,17 @@
 #include <math.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 
 float* buffer1, *buffer2, *buffer3, *N;
 
 #ifdef MATLAB_MEX_FILE  // if compiled as a MEX-file
 #include "mex.h"
 #include <time.h>
+#include "GuidedFilter.h"
 
-// mex call
-// output_image = FastGlobalSmoothing(input_image, sigma, lambda, solver_iteration);
+// Guided filtering MEX-gateway
+// output_image = GuidedFilter(input_image, radius, epsilon);
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
@@ -18,12 +20,31 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         mexErrMsgTxt("GF must be called with 3 arguments.");
     }
   
-    const mxArray *img = prhs[0]; // input images
+    const mxArray *img_input = prhs[0];                          // input images
+    mxArray *img_output = plhs[0] = mxDuplicateArray(img_input); // output images
+    void *ptr_input, *ptr_output;
+    mxClassID category = mxGetClassID(img_input);  
+    switch (category)
+    {
+        case mxUINT8_CLASS: 
+            ptr_input = (uint8_t*)mxGetPr(img_input); 
+            ptr_output = (uint8_t*)mxGetPr(img_output); break;
+        case mxUINT16_CLASS: 
+            ptr_input = (uint16_t*)mxGetPr(img_input); 
+            ptr_output = (uint16_t*)mxGetPr(img_output); break;    
+        case mxSINGLE_CLASS:
+            ptr_input = (float*)mxGetPr(img_input);
+            ptr_output = (float*)mxGetPr(img_output); break;
+        case mxDOUBLE_CLASS: 
+            ptr_input = (double*)mxGetPr(img_input);
+            ptr_output = (double*)mxGetPr(img_output); break;
+        default: mexErrMsgTxt("Expected input image to be one of these types:\n\nuint8, uint16, single, double");
+    }
 
     // image resolution
-    int width = mxGetDimensions(img)[1];
-    int height = mxGetDimensions(img)[0];
-    int nChannels = (mxGetNumberOfDimensions(img) > 2)?(mxGetDimensions(img)[2]):1;
+    int width = mxGetDimensions(img_input)[1];
+    int height = mxGetDimensions(img_input)[0];
+    int nChannels = (mxGetNumberOfDimensions(img_input) > 2)?(mxGetDimensions(img_input)[2]):1;
     
     // Guided filter parameters
     int radius = mxGetScalar(prhs[1]);
@@ -31,16 +52,45 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
     mexPrintf("Image resolution: %d x %d x %d\n", width, height, nChannels);
     mexPrintf("Parameters:\n");
-    mexPrintf("    radius = %f\n", radius);
+    mexPrintf("    radius = %d\n", radius);
     mexPrintf("    epsilon = %f\n", epsilon);
     
     // Image buffer preperation
     float* image = (float*)malloc(width*height*nChannels*sizeof(float));
-    double* ptr_input = (double*)mxGetPr(img);
-    for (int i = 0; i < height; ++i)
-        for (int j = 0; j < width; ++j)
-            for (int c = 0; c < nChannels; ++c)
-                image[c*height*width + i*width + j] = (float)(ptr_input[j*height + i + c*height*width]);
+
+    switch (category)
+    {
+        case mxUINT8_CLASS:
+            for (int i = 0; i < height; ++i)
+                for (int j = 0; j < width; ++j)
+                    for (int c = 0; c < nChannels; ++c)
+                        image[c*height*width + i*width + j] = (float)(((uint8_t*)ptr_input)[j*height + i + c*height*width])/255;
+            break;
+        case mxUINT16_CLASS:
+            for (int i = 0; i < height; ++i)
+                for (int j = 0; j < width; ++j)
+                    for (int c = 0; c < nChannels; ++c)
+                        image[c*height*width + i*width + j] = (float)(((uint16_t*)ptr_input)[j*height + i + c*height*width])/65535;
+            break;
+        case mxSINGLE_CLASS:
+            for (int i = 0; i < height; ++i)
+                for (int j = 0; j < width; ++j)
+                    for (int c = 0; c < nChannels; ++c)
+                        image[c*height*width + i*width + j] = (float)(((float*)ptr_input)[j*height + i + c*height*width]);
+            break;
+        case mxDOUBLE_CLASS:
+            for (int i = 0; i < height; ++i)
+                for (int j = 0; j < width; ++j)
+                    for (int c = 0; c < nChannels; ++c)
+                        image[c*height*width + i*width + j] = (float)(((double*)ptr_input)[j*height + i + c*height*width]);
+            break;
+    }
+
+    for(int i = 0;i < height*width*nChannels; ++i)
+    {
+        if (image[i]*(image[i] - 1.0) > 0.0)
+            mexErrMsgTxt("Intensity value must be between 0 and 1.");
+    }
     
     InitGF(width, height, radius);
 
@@ -53,28 +103,37 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
     DeinitGF();
 
-    // output
-    mxArray *image_result = plhs[0] = mxDuplicateArray(img);
-    double* ptr_output = (double*)mxGetPr(image_result);
-
-    for (int i = 0; i < height; ++i)
-        for (int j = 0; j < width; ++j)
-            for (int c = 0; c < nChannels; ++c)
-                ptr_output[j*height + i + c*height*width] = (double)(image[c*height*width + i*width + j]);
-
+    // output conversion
+    switch (category)
+    {
+        case mxUINT8_CLASS:
+            for (int i = 0; i < height; ++i)
+                for (int j = 0; j < width; ++j)
+                    for (int c = 0; c < nChannels; ++c)
+                        ((uint8_t*)ptr_output)[j*height + i + c*height*width] = (uint8_t)(image[c*height*width + i*width + j]*255 + 0.5);
+            break;
+        case mxUINT16_CLASS:
+            for (int i = 0; i < height; ++i)
+                for (int j = 0; j < width; ++j)
+                    for (int c = 0; c < nChannels; ++c)
+                        ((uint16_t*)ptr_output)[j*height + i + c*height*width] = (uint16_t)(image[c*height*width + i*width + j]*65535 + 0.5);
+            break;
+        case mxSINGLE_CLASS:
+            for (int i = 0; i < height; ++i)
+                for (int j = 0; j < width; ++j)
+                    for (int c = 0; c < nChannels; ++c)
+                        ((float*)ptr_output)[j*height + i + c*height*width] = (float)(image[c*height*width + i*width + j]);
+            break;
+        case mxDOUBLE_CLASS:
+            for (int i = 0; i < height; ++i)
+                for (int j = 0; j < width; ++j)
+                    for (int c = 0; c < nChannels; ++c)
+                        ((double*)ptr_output)[j*height + i + c*height*width] = (double)(image[c*height*width + i*width + j]);    
+            break;    
+    }
     free(image);
 }
 #endif // MATLAB_MEX_FILE
-
-/*
- *    Guided Filter Routine
- *    
- *    image                         image buffer of size width*height (float)
- *    width                         image width
- *    height                        image height
- *    radius                        local window radius
- *    epsilon                       regularization parameter
-*/
 
 int BoxFilter(float* dst, const float* src, int width, int height, int r)
 {
@@ -134,10 +193,32 @@ int BoxFilter(float* dst, const float* src, int width, int height, int r)
     return 0;
 }
 
+/*
+ *    Guided filtering Routine
+ *    
+ *    image                         image buffer of size width*height (float)
+ *    width                         image width
+ *    height                        image height
+ *    radius                        neighborbood radius
+ *    epsilon                       variance threshold
+*/
+
 int GuidedFilter(float* image, int width, int height, int radius, float epsilon)
 {
-    int sz = width*height;
-    int px;
+    int px, sz = width*height;
+
+    if (image == NULL)
+    {
+        return 1;
+    }
+
+    for (px = 0; px < sz; ++px)
+    {
+        // restrict the pixel value within 0 and 1
+        if (image[px]*(image[px] - 1.0) > 0.0)
+            return 1;
+    }
+
     for (px = 0; px < sz; ++px)
     {
         buffer1[px] = image[px]*image[px];
@@ -174,6 +255,15 @@ int GuidedFilter(float* image, int width, int height, int radius, float epsilon)
     for (px = 0; px < sz; ++px)
     {
         image[px] = buffer3[px]*image[px] + buffer2[px];
+    }
+
+    // clip the value
+    for (px = 0; px < sz; ++px)
+    {
+        if(image[px] < 0.0)
+            image[px] = 0.0;
+        if(image[px] > 1.0)
+            image[px] = 1.0;
     }
     return 0;
 }
