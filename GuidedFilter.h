a2e941985864bfe8b4015ee88a01972fd5c5af98
gary21978 <gary21978@126.com>
2020-09-28 14:43:55 +0800

    comments

--- GuidedFilter.h
+++ GuidedFilter.h
@@ -1,6 +1,8 @@
 #ifndef _GF_H_
 #define _GF_H_
 
+// O(1) time implementation of guided filter
+
 int GuidedFilter(float* image, int width, int height, int radius, float epsilon);
 int InitGF(int width, int height, int radius);
 int DeinitGF();
